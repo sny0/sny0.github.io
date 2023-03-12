d8c6125110452654969dacf1e12df9eb83c1c7ef
sny0 <shinya31415926535@gmail.com>
2023-03-12 20:39:01 +0900

    github用に変更

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,15 +11,15 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://sny0.github.io/my-docusaurus/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: 'my-docusaurus',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'sny0', // Usually your GitHub org/user name.
+  projectName: 'my-docusaurus', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
