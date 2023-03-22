978b19c5c5591595c9421b8fdf4c33142e57cc76
sny0 <shinya31415926535@gmail.com>
2023-03-22 15:43:13 +0900

    halloMarkdownを削除

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,7 +5,7 @@ const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 module.exports= {
-title: 'sny0',
+title: 'My Site',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
